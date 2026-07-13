4118dd1936fb5a2f3a5382af25f748ff8c110815
antbeltran <anthonybltrn@gmail.com>
2026-07-13 14:11:28 -0500

    Initial commit

---
+++ src/main/java/com/yearup/SkillsSprint_accountingledger/SkillsSprintAccountingledgerApplication.java
@@ -0,0 +1,13 @@
+package com.yearup.SkillsSprint_accountingledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillsSprintAccountingledgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillsSprintAccountingledgerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yearup/SkillsSprint_accountingledger/SkillsSprintAccountingledgerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yearup.SkillsSprint_accountingledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillsSprintAccountingledgerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
