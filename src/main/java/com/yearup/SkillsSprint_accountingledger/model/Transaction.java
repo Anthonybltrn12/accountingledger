d397dc5a541e460decfe83b879928c00935eae6a
antbeltran <anthonybltrn@gmail.com>
2026-07-13 14:54:12 -0500

    app is connected to database and the transaction model is created to work from

---
+++ src/main/java/com/yearup/SkillsSprint_accountingledger/model/Transaction.java
@@ -0,0 +1,55 @@
+package com.yearup.SkillsSprint_accountingledger.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="transactions")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="transactionId")
+    private int transactionId;
+
+    private String description;
+
+    private String vendor;
+
+    private int amount;
+
+    public Transaction(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
