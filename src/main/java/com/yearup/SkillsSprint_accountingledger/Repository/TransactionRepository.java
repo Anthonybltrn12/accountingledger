2783c21b11f67562d9112fb720f54507a2531f40
antbeltran <anthonybltrn@gmail.com>
2026-07-13 20:00:54 -0500

    created transaction repository

---
+++ src/main/java/com/yearup/SkillsSprint_accountingledger/Repository/TransactionRepository.java
@@ -0,0 +1,12 @@
+package com.yearup.SkillsSprint_accountingledger.Repository;
+
+import com.yearup.SkillsSprint_accountingledger.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction,Integer> {
+
+}
